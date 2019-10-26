395cbc92d06a7193d33777581853cca261afcef4
Diego Doná <diedona@gmail.com>
2019-10-26 20:11:55 -0300

    refactoring acai

--- DDona.Admister.PedidosContext.Domain/Entities/Acai.cs
+++ DDona.Admister.PedidosContext.Domain/Entities/Acai.cs
@@ -11,14 +11,30 @@ namespace DDona.Admister.PedidosContext.Domain.Entities
     {
         private readonly IList<Complemento> _complementos;
 
-        public Acai(ETamanhoAcai tamanho, IList<Complemento> complementos)
+        public Acai(ETamanhoAcai tamanho)
         {
             Tamanho = tamanho;
-            _complementos = complementos;
+            _complementos = new List<Complemento>();
         }
 
         public ETamanhoAcai Tamanho { get; private set; }
         public IReadOnlyCollection<Complemento> Complementos { get { return _complementos.ToArray(); } }
 
+        public void AddComplemento(Complemento complemento)
+        {
+            // não pode adicionar 2x o mesmo complemento
+            if(_complementos.Any(x => x.Id.Equals(complemento.Id)))
+            {
+                // notify
+            }
+
+            // não pode ter mais que 5 complementos
+            if(_complementos.Count >= 5)
+            {
+                // notify
+            }
+
+            _complementos.Add(complemento);
+        }
     }
 }
